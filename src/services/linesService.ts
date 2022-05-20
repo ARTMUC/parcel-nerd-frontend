fae5af7b6c02dd73a9e9b0d3d3ff4c5bd09ca031
ARTMUC <89223118+ARTMUC@users.noreply.github.com>
2022-05-20 10:36:09 +0200

    Create linesService.ts

---
+++ src/services/linesService.ts
@@ -0,0 +1,67 @@
+import { CreateLine } from '../interfaces/createLine.type';
+import { CreateProject } from '../interfaces/createProject.interface';
+import { Line } from '../interfaces/line.type';
+
+import { Project } from '../interfaces/project.interface';
+import { ServerError } from '../interfaces/server-error.interface';
+import { API_URL, defaultErrMessage, FetchError, mapServerResponse } from './fetchUtils';
+
+export const getAllLines = async (projectId: string): Promise<Line[] | void> => {
+  try {
+    const response = await fetch(`${API_URL}lines/project=${projectId}`, { credentials: 'include' });
+    const body: Line[] | ServerError = await response.json();
+
+    return mapServerResponse<Line[]>(response, body);
+  } catch (error: unknown) {
+    if (error instanceof FetchError) {
+      throw error;
+    }
+    throw new Error(defaultErrMessage);
+  }
+};
+
+export const addLine = async (createLine: CreateLine, projectId: string): Promise<Line | void> => {
+  try {
+    const response = await fetch(`${API_URL}lines/project=${projectId}`, {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(createLine)
+    });
+    const body: Line | ServerError = await response.json();
+
+    return mapServerResponse<Line>(response, body);
+  } catch (error: unknown) {
+    if (error instanceof FetchError) {
+      throw error;
+    }
+    throw new Error(defaultErrMessage);
+  }
+};
+
+// export const patchProject = async (project: Project): Promise<{ count: number } | void> => {
+//   try {
+//     const { id, title, content } = project;
+//     const response = await fetch(`${API_URL}projects/${id}`, {
+//       credentials: 'include',
+//       method: 'PATCH',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({ title, content })
+//     });
+//     const body: { count: number } | ServerError = await response.json();
+
+//     return mapServerResponse<{ count: number }>(response, body);
+//   } catch (error: unknown) {
+//     if (error instanceof FetchError) {
+//       throw error;
+//     }
+//     throw new Error(defaultErrMessage);
+//   }
+// };
+//
+// @TODO: implement remove and patch
+//
